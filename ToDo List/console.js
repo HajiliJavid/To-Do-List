f6442974cc639cf3eff21ff71361e1ef33caa37c
Javid Hajili <145369360+HajiliJavid@users.noreply.github.com>
2024-09-24 14:48:11 +0400

    Add files via upload

---
+++ ToDo List/console.js
@@ -0,0 +1,33 @@
+const tasksContainer = document.querySelector(".tasks");
+const addTaskButton = document.querySelector(".newTask button");
+const taskInput = document.querySelector(".newTask input");
+const delButton = document.querySelectorAll(".task button");
+
+const addTask = () => {
+  const taskText = taskInput.value.trim();
+  if (taskText === "") 
+    return;
+  const newTask = document.createElement("div");
+  newTask.classList.add("task");
+  newTask.innerHTML = `
+        <span>${taskText}</span>
+        <button><i class="fa-solid fa-trash"></i></button>
+    `;
+  tasksContainer.appendChild(newTask);
+  taskInput.value = "";
+};
+
+addTaskButton.addEventListener("click", addTask);
+
+tasksContainer.addEventListener("click", (e) => {
+    let targetElement = null
+    if (e.target.tagName == 'BUTTON') {
+        targetElement = e.target;
+    } else if (e.target.tagName == 'I') {
+        targetElement = e.target.parentElement
+    } else {
+        return
+    }
+    const taskToRemove = targetElement.parentElement;
+    tasksContainer.removeChild(taskToRemove);
+})
